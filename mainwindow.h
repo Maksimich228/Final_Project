e3d34f9a2d12def57abffa2e1d01633460c16eb8
Maksimich228 <147976922+Maksimich228@users.noreply.github.com>
2024-10-02 21:58:54 +0300

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,57 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QListWidgetItem>
+#include <string>
+#include <vector>
+
+namespace Ui {
+class MainWindow;
+}
+
+struct User {
+    std::string username;
+    bool isBanned;
+};
+
+struct Message {
+    std::string sender;
+    std::string content;
+};
+
+class Messenger {
+public:
+    Messenger(); // Объявление конструктора
+    void addUser(const std::string& username);
+    void addMessage(const std::string& sender, const std::string& content);
+    void banUser(const std::string& username);
+
+    std::vector<User> users;
+    std::vector<Message> messages;
+};
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_sendButton_clicked();
+    void on_loginButton_clicked();
+    void on_userListWidget_itemClicked(QListWidgetItem* item);
+    void on_banButton_clicked();
+
+private:
+    void updateUserList();
+    void updateMessageList();
+
+    Ui::MainWindow *ui;
+    Messenger messenger;
+    std::string currentUser;
+};
+
+#endif // MAINWINDOW_H
+
